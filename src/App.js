ea07cfda3405c44436e32a483ab0d44965e2502e
Bubumariia Kudaiberdieva <mariiakudaiberdieva@gmail.com>
2022-10-06 14:42:27 +0300

    added functionalities to buttons and worked with sessionStorage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
+import { v4 as uuidv4 } from "uuid";
+import { setSelectionRange } from "@testing-library/user-event/dist/utils";
 
 export default class App extends Component {
   constructor() {
@@ -11,6 +13,12 @@ export default class App extends Component {
       userName: "",
       errorMessage: "",
     };
+  }
+  componentDidMount() {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      this.setState({ loggedIn: true });
+    }
   }
   handleFormSubmit = (e) => {
     e.preventDefault();
@@ -27,30 +35,35 @@ export default class App extends Component {
       this.setState({ fiedsEmpty: true });
       setTimeout(() => {
         this.setState({ fiedsEmpty: false });
-      }, 3000);
+      }, 1000);
     }
   };
+
   authenticateUser = async (creds) => {
     const { email, password } = creds;
 
     const url = "https://633d68f37e19b178290dd4a7.mockapi.io/api/v1/users";
     const response = await fetch(url);
-
     const data = await response.json();
+    const token = uuidv4();
     const user = data.find((el, index) =>
       el.email === email && el.password === password
-        ? this.setState({ loggedIn: true, userName: data[index].fname })
-        : this.setState({ errorMessage: "Username or Password is incorrect" })
+        ? (this.setState({ loggedIn: true, userName: data[index].fname }),
+          sessionStorage.setItem("token", token))
+        : null
     );
-    console.log(this.state.userName);
-    console.log(data);
+  };
+  handleLogout = () => {
+    this.setState({ loggedIn: false });
+    sessionStorage.clear();
   };
   render() {
+    console.log(this.props.logout);
     const { loggedIn, fiedsEmpty, userName, errorMessage } = this.state;
     return (
       <div className="App">
         {loggedIn ? (
-          <Dashboard userName={userName} />
+          <Dashboard userName={userName} handleLogout={this.handleLogout} />
         ) : (
           <Login
             handleFormSubmit={this.handleFormSubmit}
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 import { Button } from "reactstrap";
 import "./styles/Dashboard.css";
+import Login from "./Login";
 
 export default class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      logout: true,
+    };
+  }
+  handleClick = () => {
+    this.setState({ logout: true });
+  };
+
   render() {
     return (
       <div className="dashboard">
         <h2>Dashboard Page</h2>
         <p>Dear {this.props.userName}, welcome to your dashboard.</p>
-        <Button>Logout</Button>
+        <Button onClick={this.props.handleLogout}>Logout</Button>
       </div>
     );
   }
